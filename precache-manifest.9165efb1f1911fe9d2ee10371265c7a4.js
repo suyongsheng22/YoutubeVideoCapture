31a7a5ff602e9ec2e6fdc8076a85d8c746386a42
jordansu <jdsu1996@gmail.com>
2020-09-08 10:27:37 +0800

    Updates

---
+++ precache-manifest.9165efb1f1911fe9d2ee10371265c7a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502415df17d9026d5b71958b1e43c246",
+    "url": "/YoutubeVideoCapture/index.html"
+  },
+  {
+    "revision": "f3d2fefc669762edae41",
+    "url": "/YoutubeVideoCapture/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "4eb0b234b5ea72950b4a",
+    "url": "/YoutubeVideoCapture/static/css/main.fa283869.chunk.css"
+  },
+  {
+    "revision": "f3d2fefc669762edae41",
+    "url": "/YoutubeVideoCapture/static/js/2.0de7eaa1.chunk.js"
+  },
+  {
+    "revision": "6c99299aa0ccf7cd363681b2638ef8ef",
+    "url": "/YoutubeVideoCapture/static/js/2.0de7eaa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eb0b234b5ea72950b4a",
+    "url": "/YoutubeVideoCapture/static/js/main.3c977cec.chunk.js"
+  },
+  {
+    "revision": "57cf4d906a050a912d45",
+    "url": "/YoutubeVideoCapture/static/js/runtime-main.31033566.js"
+  },
+  {
+    "revision": "f0cd583089ee79b8be7c3084d1219c50",
+    "url": "/YoutubeVideoCapture/static/media/Loading.f0cd5830.gif"
+  },
+  {
+    "revision": "79901f596e13193089bdda1f5352d836",
+    "url": "/YoutubeVideoCapture/static/media/NC-left.79901f59.jpg"
+  },
+  {
+    "revision": "7605fce8a7b1654e828a2dce2bb00db2",
+    "url": "/YoutubeVideoCapture/static/media/NC-right.7605fce8.jpg"
+  }
+]);
